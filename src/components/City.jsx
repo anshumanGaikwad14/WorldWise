f05bfd835199a9e820e5b37a6bed4550b069fdc9
anshumanGaikwad14 <gaanshuman14@gmail.com>
2026-09-19 21:41:08 +0530

    done with the navigation ui

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import styles from "./City.module.css";
-import { useParams, useSearchParams } from "react-router-dom";
-import { CitiesProvider, useCities } from "../contexts/CitiesContext";
+import { useParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 import Spinner from "./Spinner";
+
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -67,8 +69,9 @@ function City() {
           Check out {cityName} on Wikipedia &rarr;
         </a>
       </div>
-
-      <div>{/* <ButtonBack /> */}</div>
+      <div>
+        <BackButton />
+      </div>
     </div>
   );
 }
